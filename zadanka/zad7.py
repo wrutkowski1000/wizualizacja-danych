95a5b2ac620bc771c7a20d46277a5a35ff5c4fb7
wrutkowski1000 <60234946+wrutkowski1000@users.noreply.github.com>
2020-04-14 21:18:18 +0200

    robal

--- zadanka/zad7.py
+++ zadanka/zad7.py
@@ -1,21 +1,25 @@
-class Robaczek :
+class Robal:
     def init (self, rx, ry, rkrok):
         self.x = rx
         self.y = ry
         self.krok = rkrok
-    def idz_w_gore(self, ile_krokow):
-        self.y = self.y + ile_krokow * self.krok
-    def idz_w_dol(self, ile_krokow):
-        self.y = self.y - ile_krokow * self.krok
-    def idz_w_lewo(self, ile_krokow):
-        self.x = self.x - ile_krokow * self.krok
-    def idz_w_prawo(self, ile_krokow):
-        self.x = self.x + ile_krokow * self.krok
+
+    def idz_w_gore(self, kroki):
+        self.y = self.y + kroki * self.krok
+
+    def idz_w_dol(self, kroki):
+        self.y = self.y - kroki * self.krok
+
+    def idz_w_lewo(self, kroki):
+        self.x = self.x - kroki * self.krok
+
+    def idz_w_prawo(self, kroki):
+        self.x = self.x + kroki * self.krok
 
     def pokaz_gdzie_jestes(self) :
         print("x={}, y={}".format(self.x, self.y))
 
-robal = Robaczek()
+robal = Robal(1,2,5)
 robal.idz_w_gore(3)
 robal.idz_w_dol(1)
 robal.idz_w_lewo(3)
