c989ba83837ab6ff8dead05700bfdb5bd18e8205
wrutkowski1000 <60234946+wrutkowski1000@users.noreply.github.com>
2020-04-26 15:41:03 +0200

    l5zad1

---
+++ zadanka/l5zad1.py
@@ -0,0 +1,29 @@
+class Material:
+
+    rodzaj = ""
+    dlugosc = 0
+    szerokosc = 0
+
+    def __init__(self, r, d, s):
+        self.rodzaj = r
+        self.dlugosc = d
+        self.szerokosc = s
+
+    def wyswietl_nazwe(self):
+        print(self.rodzaj)
+
+class Ubrania:
+    rozmiar = 0
+    kolor = ""
+    dla_kogo = ""
+
+    def wyswietl_dane(self):
+        print("rozmiar: " ,self.rozmiar , " , kolor: " , self.kolor , " , dla: " , self.dla_kogo);
+
+class Sweter:
+    
+ubranko = Ubrania()
+ubranko.dla_kogo = "dla mnie"
+ubranko.kolor = "czarny"
+ubranko.rozmiar = 10
+ubranko.wyswietl_dane()
