dee63dff000c0184ba74ba689a68425eea3001ab
wrutkowski1000 <60234946+wrutkowski1000@users.noreply.github.com>
2020-04-26 15:55:12 +0200

    l5zad1deluxeedition

--- zadanka/l5zad1.py
+++ zadanka/l5zad1.py
@@ -12,22 +12,37 @@ class Material:
     def wyswietl_nazwe(self):
         print(self.rodzaj)
 
-class Ubrania:
+class Ubrania(Material):
     rozmiar = 0
     kolor = ""
     dla_kogo = ""
 
+    def __init__(self, r, d, s, roz, kolo, dla):
+        self.rodzaj = r
+        self.dlugosc = d
+        self.szerokosc = s
+        self.rozmiar = roz
+        self.kolor = kolo
+        self.dla_kogo = dla
+
     def wyswietl_dane(self):
         print("rozmiar: " ,self.rozmiar , " , kolor: " , self.kolor , " , dla: " , self.dla_kogo);
 
-class Sweter:
+class Sweter(Ubrania):
     rodzaj_swetra = ""
+
+    def __init__(self, d, s, roz, kolo, dla, rswetra):
+        self.rodzaj = "sweter"
+        self.dlugosc = d
+        self.szerokosc = s
+        self.rozmiar = roz
+        self.kolor = kolo
+        self.dla_kogo = dla
+        self.rodzaj_swetra = rswetra
+
     def wyswietl_dane(self):
         print("rozmiar: ", self.rozmiar, " , kolor: ", self.kolor, " , dla: ", self.dla_kogo, " rodzaj swetra: ", self.rodzaj_swetra);
 
-        
-ubranko = Ubrania()
-ubranko.dla_kogo = "dla mnie"
-ubranko.kolor = "czarny"
-ubranko.rozmiar = 10
+
+ubranko = Sweter(10, 50, 15, "czarny", "dla mnie","bawelniany")
 ubranko.wyswietl_dane()
