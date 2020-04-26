23f348846d10315c5702e0e395c9f14fc984589d
wrutkowski1000 <60234946+wrutkowski1000@users.noreply.github.com>
2020-04-26 20:50:04 +0200

    l5zad7

---
+++ zadanka/l5zad6.py
@@ -0,0 +1,19 @@
+class Wspak:
+    """Iterator zwracający wartości w odwróconym porządku"""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+nazwa = Wspak("ABCDEFGHIJ")
+n = iter(nazwa)
+for i in range(0,10):
+    print(next(n))
